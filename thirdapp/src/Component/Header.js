35c1f2f29ce405621c768fc758ac4f3936b2e13f
aakashdeveloper <ahanda205@gmail.com>
2021-02-17 06:48:52 +0530

    s

--- thirdapp/src/Component/Header.js
+++ thirdapp/src/Component/Header.js
@@ -1,33 +1,88 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{Component} from 'react';
+import {Link,withRouter} from 'react-router-dom';
 
-const Header = () =>{
-    return(
-        <div>
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Developer Funnel</a>
+class Header extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            username:'',
+            imgurl:''
+        }
+    }
+
+    conditionalHeader =() => {
+        if(sessionStorage.getItem('username') == null || sessionStorage.getItem('username') == undefined){
+            return(
+                <li>
+                    <a href="https://github.com/login/oauth/authorize?client_id=841775ffdee13afd7f4f">
+                        Login With Github
+                    </a>
+                </li>
+            )
+        }else{
+            return(
+                <li>
+                    <a href="">
+                        <img src={this.state.imgurl} style={{height:50,width:50}}/>
+                        Hi {sessionStorage.getItem('username')}
+                    </a>
+                </li>
+            )
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Developer Funnel</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/viewBooking">Bookings</Link></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            {this.conditionalHeader()}
+                        </ul>
+                        </div>
                     </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/viewBooking">Bookings</Link></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    )
+                </nav>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        const code = (this.props.location.search).split('=')[1];
+        if(code){
+            let requestData={
+                code:code
+            }
+            fetch(`http://localhost:9800/oauth`,{
+                method:'POST',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(requestData)
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                var result = data.login;
+                var img = data.avatar_url;
+                sessionStorage.setItem('username',result)
+                this.setState({imgurl:img,username:result})
+            })
+        }
+    }
 }
 
-export default Header
+export default withRouter(Header)
