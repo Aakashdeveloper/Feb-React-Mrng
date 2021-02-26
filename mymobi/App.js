a4d57881a5941abe6961afa57cd237b130d9ff00
aakashdeveloper <ahanda205@gmail.com>
2021-02-26 06:52:40 +0530

    s

--- mymobi/App.js
+++ mymobi/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import ProductList from './Component/productList';
 
 export default function App() {
+  const [OutputText,setText] = useState("Test Native Button")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{OutputText}</Text>
+      <Button onPress={() => {setText('Text Changed')}} title="Click Me"/>
+      <Button onPress={() => {setText('Test Native Button')}} title="Revert Me"/>
+      <ProductList/>
       <StatusBar style="auto" />
     </View>
   );
